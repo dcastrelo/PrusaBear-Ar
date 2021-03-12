b2936701c0bc118ab32a160d5edbe32ee9a293ec
dcastrelo <castrelo@gmail.com>
2021-03-12 17:34:32 -0300

    Update Configuration.h

--- Marlin 2.0.7.2 Upgrade V1 a V1.5/Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin 2.0.7.2 Upgrade V1 a V1.5/Marlin-2.0.x/Marlin/Configuration.h
@@ -103,7 +103,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "KOI3D BMG V2"
+#define CUSTOM_MACHINE_NAME "Prusa Bear V1.5"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
