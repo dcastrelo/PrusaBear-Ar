78962b20d58aa09cc8b05496a39fb990c43b692e
dcastrelo <castrelo@gmail.com>
2021-03-23 10:19:15 -0300

    d

--- Marlin-2.0.7.2 V2 arreglo PLR/Marlin/Configuration.h
+++ Marlin-2.0.7.2 V2 arreglo PLR/Marlin/Configuration.h
@@ -1097,8 +1097,8 @@
 #define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -10
-#define Y_MIN_POS -4
+#define X_MIN_POS -12
+#define Y_MIN_POS -6
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
